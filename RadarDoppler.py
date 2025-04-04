10e0ff52452e5465e9d879e2d683033bcbb350bc
HasanMuhammadAli <mali.hasan.612@gmail.com>
2025-04-04 21:06:31 +0530

    updated code for doppler effect

--- RadarDoppler.py
+++ RadarDoppler.py
@@ -1,9 +1,28 @@
-#Frequency shift by moving object
+# Doppler Shift Calculation for a Moving Object
+# Author: Ali Mosin Hasan
+# Date: 03/04/2025
+# Description: This program calculates the Doppler frequency shift
+#              when an object moves towards a radar system.
 
-c = 3e8 #speed of light
-f_transmit = 10e9 #Radar frequency 10GHz
+# Constants
+c = 3e8  # Speed of light in meters per second (m/s)
+f_transmit = 10e9  # Radar transmitted frequency in Hz (10 GHz)
+
 def doppler_shift(velocity):
-    return (2*velocity*f_transmit)/c
-velocity = 30 #Assume the object is moving towards radar m/s
+    """
+    Calculates the Doppler shift frequency due to a moving object.
+    
+    Parameters:
+    velocity (float): The speed of the object in meters per second (m/s).
+    
+    Returns:
+    float: The Doppler shift in frequency (Hz).
+    """
+    return (2 * velocity * f_transmit) / c
+
+# Example: Object moving towards the radar at 50 m/s
+velocity = 50  # Velocity of the moving object in m/s
 doppler_frequency = doppler_shift(velocity)
+
+# Output the result
 print(f"Doppler Shift: {doppler_frequency:.2f} Hz")
